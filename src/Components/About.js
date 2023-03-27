9af339fd24c2c88e0c42465c022b24586c1c0b3b
Johnny Tang <email.john.e@gmail.com>
2023-03-26 23:19:56 -0700

    Update About.js
    remove unused varaibles

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,9 +7,6 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
     }
