149e1e9d1e9fcb81654897d5c40f6e328f023f5d
Johnny Tang <email.john.e@gmail.com>
2023-03-26 23:04:49 -0700

    Update About.js
    remove address

--- src/Components/About.js
+++ src/Components/About.js
@@ -29,9 +29,6 @@ class About extends Component {
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
-						   <span>
-						         {city} {state}, {zip}
-                   </span><br />
 						   <span>{phone}</span><br />
                      <span>{email}</span>
 					   </p>
